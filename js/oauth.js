b836248fd8b7bedb1420ca826eecf356d80c40d7
Soraa <soraadev@outlook.com>
2024-02-25 17:49:33 -0400

    ddddd

--- js/oauth.js
+++ js/oauth.js
@@ -1,5 +1,5 @@
-clientId = "";
-redirectUri = "";
+clientId = "60752f3b45744b0dabdd0c951db7f4c9";
+redirectUri = "localhost:5500";
 
 const spotifyUrl = `htpps://acocounts.spotify.com/authorie?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-private`;
 
